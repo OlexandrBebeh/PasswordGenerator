a848b74ea8c3275af6067fc6049c19819e29bd63
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2021-12-11 15:33:57 +0200

    Added cracked passwords

--- PasswordGenerator/Hashes/Argon2i.cs
+++ PasswordGenerator/Hashes/Argon2i.cs
@@ -13,10 +13,9 @@ namespace PasswordGenerator.Hashes
         private const int CryptoPwhashArgon2IdMemlimitSensitive = 65536;
         private const int HashLength = 256;
 
-        static Argon2i()
-        {
-            sodium_init();
-        }
+        private readonly RNGCryptoServiceProvider saltProvider = new();
+        private readonly int memLimit;
+        private readonly long opsLimit;
 
         [DllImport(Name, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void sodium_init();
@@ -25,22 +24,51 @@ namespace PasswordGenerator.Hashes
         internal static extern void randombytes_buf(byte[] buffer, int size);
 
         [DllImport(Name, CallingConvention = CallingConvention.Cdecl)]
-        internal static extern int crypto_pwhash(byte[] buffer, long bufferLen, byte[] password, long passwordLen, byte[] salt, long opsLimit, int memLimit, int alg);
+        internal static extern int crypto_pwhash(byte[] buffer, long bufferLen, byte[] password, long passwordLen,
+            byte[] salt, long opsLimit, int memLimit, int alg);
 
-        private readonly RNGCryptoServiceProvider saltProvider = new();
+        [DllImport(Name, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_pwhash_str_alg(
+            byte[] buffer, string password, long passwordLength,
+            long opsLimit, int memLimit, int alg);
+
+        [DllImport(Name, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int
+            crypto_pwhash_str_verify(string hashedPassword, string password, long passwordLength);
+
+        static Argon2i()
+        {
+            sodium_init();
+        }
+
+        public Argon2i() : this(CryptoPwhashArgon2IdMemlimitSensitive, CryptoPwhashArgon2IdOpslimitSensitive)
+        {
+        }
+
+        public Argon2i(int memLimit, long opsLimit)
+        {
+            this.memLimit = memLimit;
+            this.opsLimit = opsLimit;
+        }
+
+        public bool Verify(string password, string hashedPassword)
+        {
+            var result = crypto_pwhash_str_verify(hashedPassword, password, password.Length);
+            return result == 0;
+        }
 
         public byte[] HashPassword(string password, byte[] salt)
         {
             var hash = new byte[HashLength];
-            
+
             var result = crypto_pwhash(
                 hash,
                 hash.Length,
                 Encoding.UTF8.GetBytes(password),
                 password.Length,
                 salt,
-                CryptoPwhashArgon2IdOpslimitSensitive,
-                CryptoPwhashArgon2IdMemlimitSensitive,
+                opsLimit,
+                memLimit,
                 CryptoPwhashArgon2IdAlgArgon2Id13
             );
 
@@ -48,45 +76,37 @@ namespace PasswordGenerator.Hashes
                 throw new Exception("An unexpected error has occurred.");
 
             return hash;
-
         }
-        
+
         public byte[] HashPasswordWithSalt(string password)
         {
             var hash = new byte[HashLength];
-            
+
             var result = crypto_pwhash_str_alg(
-                hash, 
-                password, 
+                hash,
+                password,
                 password.Length,
-                CryptoPwhashArgon2IdOpslimitSensitive, 
+                CryptoPwhashArgon2IdOpslimitSensitive,
                 CryptoPwhashArgon2IdMemlimitSensitive,
                 CryptoPwhashArgon2IdAlgArgon2Id13);
 
             if (result != 0)
                 throw new Exception("An unexpected error has occurred.");
-            
+
             int lastIndex = Array.FindLastIndex(hash, b => b != 0);
 
             Array.Resize(ref hash, lastIndex + 1);
 
             return hash;
-
         }
-        
+
         public byte[] GenerateRandomSequence(int length)
         {
             byte[] bytes = new byte[length];
-            
+
             saltProvider.GetNonZeroBytes(bytes);
-            
+
             return bytes;
         }
-        
-        [DllImport(Name, CallingConvention = CallingConvention.Cdecl)]
-        internal static extern int crypto_pwhash_str_alg(
-            byte[] buffer, string password, long passwordLength,
-            long opsLimit, int memLimit, int alg);
     }
-    
 }
--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using PasswordGenerator.Hashes;
 using PasswordGenerator.PasswordCreator;
 
 class Program
 {
+    private const int NumberOfThread = 16;
+    private static object locker = new();
+
     static void Main()
     {
-        List<IPasswordCreator> lst = new ()
+        // GeneratePasswords();
+        TryBreakArgon();
+    }
+
+    private static void GeneratePasswords()
+    {
+        List<IPasswordCreator> lst = new()
         {
-            new HumanPasswordsCreator("../../../Resources/millionpasswords.txt"),
-            new HumanPasswordsCreator("../../../Resources/50passwords.txt"),
+            new HumanPasswordsCreator(GetPathInProject("Resources/millionpasswords.txt")),
+            new HumanPasswordsCreator(GetPathInProject("Resources/50passwords.txt")),
             new RandomPasswordCreator(),
             new RulePasswordCreator()
         };
 
         List<string> pwd = new();
-        
+
         pwd.AddRange(lst[0].GeneratePasswords(10000));
         pwd.AddRange(lst[1].GeneratePasswords(10000));
         pwd.AddRange(lst[2].GeneratePasswords(10000));
         pwd.AddRange(lst[3].GeneratePasswords(10000));
-        
+
         var rnd = new Random();
         var orderedEnumerable = pwd.OrderBy(item => rnd.Next());
 
@@ -36,13 +46,13 @@ class Program
         foreach (var p in orderedEnumerable)
         {
             shaHashes.Add(BitConverter.ToString(SimpleSha1.GenerateHash(p)).Replace("-", ""));
-            
+
             var salt = argon.GenerateRandomSequence(16);
             var saltHex = BitConverter.ToString(salt).Replace("-", "");
             var hashedHex = BitConverter.ToString(argon.HashPassword(p, salt)).Replace("-", "");
-            
+
             argonHashes.Add($"{Encoding.Default.GetString(argon.HashPasswordWithSalt(p))}");
-            
+
             saltedHashes.Add($"{hashedHex};{saltHex}");
         }
 
@@ -50,6 +60,82 @@ class Program
         File.WriteAllLines("hashes.csv", shaHashes);
         File.WriteAllLines("saltedhashes.csv", saltedHashes);
         File.WriteAllLines("argonhashes.csv", argonHashes);
+    }
 
+    private static void TryBreakArgon()
+    {
+        var argon = new Argon2i(4096 * 1024, 3);
+        var passwords = ReadPasswords();
+        var hashes = ReadHashes();
+        var foundPasswords = new Dictionary<string, string>();
+        var threads = new Thread[NumberOfThread];
+        var numberOfPasswords = passwords.Length / NumberOfThread;
+        var extra = passwords.Length % NumberOfThread;
+        for (var i = 0; i < NumberOfThread; i++)
+        {
+            var number = i < extra ? numberOfPasswords + 1 : numberOfPasswords;
+            var skippedPasswords = i * numberOfPasswords + (extra - Math.Max(0, extra - i));
+            var threadPasswords = passwords.Skip(skippedPasswords).Take(number);
+            threads[i] = new Thread(() => ProcessPasswords(argon, threadPasswords, hashes, foundPasswords));
+            threads[i].Start();
+        }
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        var builder = new StringBuilder();
+        foreach (var (h, p) in foundPasswords)
+        {
+            builder.Append(h);
+            builder.Append(" -> ");
+            builder.Append(p);
+        }
+
+        Console.WriteLine($"Found: {foundPasswords.Count}");
+        File.WriteAllText(GetPathInProject("Resources/strong_cracked.txt"), builder.ToString());
+    }
+
+    private static void ProcessPasswords(Argon2i argon, IEnumerable<string> passwords, string[] hashes,
+        Dictionary<string, string> foundPasswords)
+    {
+        foreach (var password in passwords)
+        {
+            Console.WriteLine($"Processing {password}");
+            foreach (var hash in hashes)
+            {
+                if (argon.Verify(password, hash))
+                {
+                    Console.WriteLine($"Found password: {hash} -> {password}");
+                    foundPasswords[hash] = password;
+                    Save(hash, password);
+                }
+            }
+        }
+    }
+
+    private static void Save(string hash, string password)
+    {
+        lock (locker)
+        {
+            using var w = File.AppendText(GetPathInProject("Resources/strong_cracked.txt"));
+            w.WriteLine($"{hash} -> {password}");
+            w.Flush();
+        }
+    }
+
+    private static string[] ReadPasswords()
+    {
+        return File.ReadAllLines(GetPathInProject("Resources/common_passwords.txt"));
+    }
+
+    private static string[] ReadHashes()
+    {
+        return File.ReadAllLines(GetPathInProject("Resources/strong_hashes.txt"));
+    }
+
+    private static string GetPathInProject(string path)
+    {
+        return $"../../../{path}";
     }
 }
